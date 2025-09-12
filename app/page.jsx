a2b5de98da2451fdd04b6ca7ae223aac60f47364
aniketmyana <myanaaniket@gmail.com>
2025-09-12 13:29:04 +0530

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>GitHub Demo!</h1>
+    </div>
+  );
+}
